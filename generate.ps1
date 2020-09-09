b9b64a98d624fb751a579566cb2f65cf2a6a4545
Nimaoth <nicolas.otremba@web.de>
2020-09-09 19:42:26 +0200

    stuff

--- generate.ps1
+++ generate.ps1
@@ -1,8 +1,8 @@
 New-Item -ItemType Directory -Path "int" -Force
 
-./CheezCBindingGenerator.exe vulkan.lua "./int"
+&"D:/dev/Cheez/CheezLang/Tools/CheezCBindingGenerator/bin/x64/CheezCBindingGenerator.exe" vulkan.lua "./int"
 Write-Host ""
-./CheezCBindingGenerator.exe spirv_cross.lua "./int"
+&"D:/dev/Cheez/CheezLang/Tools/CheezCBindingGenerator/bin/x64/CheezCBindingGenerator.exe" spirv_cross.lua "./int"
 Write-Host ""
 Copy-Item "./int/vulkan.che" "./vulkan/vulkan.che" -Force
 Copy-Item "./int/spirv_cross.che" "./vulkan/spirv_cross.che" -Force
